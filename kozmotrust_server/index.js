23256a6ee90e32f648f2589622849abf4c523861
gulnihal <gulnihalgezmen@gmail.com>
2024-02-14 01:23:44 +0300

    mongo connection

--- kozmotrust_server/index.js
+++ kozmotrust_server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
-const Port = process.env.port || 5000;
+mongoose.connect("mongodb://localhost:27017/myapp", { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDBb connected!");
+});
+
+const port = process.env.port || 5000;
+const app = express();
 
 app.route("/").get((req, res) => res.json("Kozmotrust API"));
 
-app.listen(5000, () => console.log(`Kozmotrust Server is running on port ${Port}!`));
+app.listen(5000, () => console.log(`Kozmotrust Server is running on port ${port}!`));
