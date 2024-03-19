88c01c4f0cac510164d8f0965053f20de0614953
Gülnihal Gezmen <gulnihalgezmen@gmail.com>
2024-03-19 04:14:37 +0300

    Update config.js

--- kozmotrust_server/config.js
+++ kozmotrust_server/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    AIapiKey: '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',
+    AIapiKey: '',
     mongoDbUrl: "mongodb+srv://Gulnihal:9639101ulya@kozmotrust.jrnnqrq.mongodb.net/kozmotrust?retryWrites=true&w=majority",
-    key: "Kozmotrust",
+    secretKey: 'Kozmotrust',
     port: 5000,
-    
 };
