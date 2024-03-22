11befb112b381ad72d465574ed81ed5726e074b1
Gülnihal Gezmen <gulnihalgezmen@gmail.com>
2024-03-23 02:48:27 +0300

    dependency added

--- kozmotrust_server/config.js
+++ kozmotrust_server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    AIapiKey: '',
+    AIapiKey: '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',
     mongoDbUrl: "mongodb+srv://Gulnihal:9639101ulya@kozmotrust.jrnnqrq.mongodb.net/kozmotrust?retryWrites=true&w=majority",
     secretKey: 'Kozmotrust',
     port: 5000,
