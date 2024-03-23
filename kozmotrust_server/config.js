e93919fd0afccbb33c4749f085139ea8e560fc77
Gülnihal Gezmen <gulnihalgezmen@gmail.com>
2024-03-23 20:27:29 +0300

    fixesse

--- kozmotrust_server/config.js
+++ kozmotrust_server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    AIapiKey: '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',
+    AIapiKey: '',
     mongoDbUrl: "mongodb+srv://Gulnihal:9639101ulya@kozmotrust.jrnnqrq.mongodb.net/kozmotrust?retryWrites=true&w=majority",
     secretKey: 'Kozmotrust',
     port: 5000,
