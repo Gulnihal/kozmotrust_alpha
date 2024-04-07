b6ca45d1b437e05a82c218d59ccf83a10e898910
ilaydaozdemir99 <ilayozdemir99@gmail.com>
2024-04-07 15:00:00 +0300

    Scan Photo from Gallery and Camera configuration added

--- kozmotrust/lib/features/camsearch/screens/cam_search_screen.dart
+++ kozmotrust/lib/features/camsearch/screens/cam_search_screen.dart
@@ -1,8 +1,11 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:kozmotrust/common/widgets/modal_dialog.dart';
 import 'package:kozmotrust/constants/global_variables.dart';
 import 'package:flutter/material.dart';
+import 'package:kozmotrust/features/camsearch/CameraUtils/image_picker_class.dart';
 
 class CameraSearchScreen extends StatefulWidget {
-  const CameraSearchScreen({super.key});
+  const CameraSearchScreen({Key? key}) : super(key: key);
 
   @override
   State<CameraSearchScreen> createState() {
@@ -23,13 +26,51 @@ class _CameraSearchScreenState extends State<CameraSearchScreen> {
           title: Image.asset('assets/images/logo.png'),
         ),
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children:[
-          ]
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  imagePickerModal(context, onCameraTap: () {
+                   pickImage(source: ImageSource.camera).then((value) {
+                     if (value != '') {
+
+                     }
+                   });
+                  } , onGalleryTap: () {
+                    pickImage(source: ImageSource.gallery).then((value) {
+                      if (value != '') {
+
+                      }
+                    });
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  child: Text(
+                    'Scan Photo',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: GlobalVariables.buttonBackgroundColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  minimumSize: Size(300, 100),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
